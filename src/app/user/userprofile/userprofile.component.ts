71cd0ec41b8dd9895b9cfa1c8293a7fefbb1981e
istiyak <istiyaq.mi9@gmail.com>
2022-07-02 15:56:47 +0530

    added ng-modal

--- src/app/user/userprofile/userprofile.component.ts
+++ src/app/user/userprofile/userprofile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import * as $ from 'JQuery';
 declare var $: any;
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-userprofile',
@@ -9,13 +10,18 @@ declare var $: any;
 })
 export class UserprofileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+  private modalReference;
 
   ngOnInit(): void {
   }
 
-  employementPopup() {
-    $('#addEmployment').modal('show');
+  employementPopup(content) {
+    // $('#addEmployment').show();
+    this.modalReference = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
   }
 
+  // closeMe() {
+  //   this.modalReference.close();
+  // }
 }
